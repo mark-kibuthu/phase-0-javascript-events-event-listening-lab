fc4c0a970e23fcd4bcc86cb6d1121b9aae42100e
Agent-grey <markkibuthu@gmail.com>
2024-07-25 15:29:42 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+
 function addingEventListener() {
-}
+    // Select the button with id 'button'
+    const input = document.getElementById('button');
+  
+    // Define the callback function
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Add the event listener to the button
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the function to set up the event listener
+  addingEventListener();
+  
